ea3ecfc8f9a8d3175712d96d6267fe6562449b8b
Sungwon Choi <csw2757@naver.com>
2023-08-05 18:41:00 +0900

    #1 init project

---
+++ src/test/groovy/com/example/refmonolithicserver/RefMonolithicServerApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.refmonolithicserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RefMonolithicServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
