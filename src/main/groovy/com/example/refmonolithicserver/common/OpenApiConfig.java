b34f4153432aaf53c981b3911d1a56b352df120b
Sungwon Choi <csw2757@naver.com>
2023-09-17 02:27:28 +0900

    #26 swagger에 토큰설정 추가

--- src/main/groovy/com/example/refmonolithicserver/common/OpenApiConfig.java
+++ src/main/groovy/com/example/refmonolithicserver/common/OpenApiConfig.java
@@ -3,6 +3,8 @@ package com.example.refmonolithicserver.common;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,8 +17,19 @@ public class OpenApiConfig {
                 .title("REF-API Definition")
                 .version("1.0")
                 .description("ref api-docs");
+
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt); // 헤더에 토큰 포함
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+
         return new OpenAPI()
-                .components(new Components())
+                .addSecurityItem(securityRequirement)
+                .components(components)
                 .info(info);
     }
 }
