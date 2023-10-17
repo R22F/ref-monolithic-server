0c5b11e684e687dfc72d44e36fc7a6d06204dce6
Sungwon Choi <csw2757@naver.com>
2023-10-18 01:01:00 +0900

    #37 예외사항 추가 정의

--- src/main/groovy/com/example/refmonolithicserver/common/exception/ExceptionController.java
+++ src/main/groovy/com/example/refmonolithicserver/common/exception/ExceptionController.java
@@ -11,12 +11,17 @@ import java.util.Arrays;
 public class ExceptionController {
 
     @ExceptionHandler(value = {BusinessException.class})
-    public ResponseEntity<ErrorResponse> exception(BusinessException e){
+    public ResponseEntity<ErrorResponse> businessException(BusinessException e){
         return ErrorResponse.toResponseEntity(e.getErrorCode(), e.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         return ErrorResponse.toResponseEntity(ErrorCode.INVALID_PARAMETER, Arrays.toString(e.getDetailMessageArguments()));
+    }
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<ErrorResponse> defaultException(Exception e){
+        return ErrorResponse.toResponseEntity(ErrorCode.INTERNAL_SERVER_ERROR, e.getMessage());
     }
 }
